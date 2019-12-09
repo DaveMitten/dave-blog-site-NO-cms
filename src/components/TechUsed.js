f0860bdee75af9efc12727a1beca9f15eda5c279
DaveMitten <davidmitten88@gmail.com>
2019-12-09 18:17:02 +0000

    resized icons, removed animation till i can reconfigure it

--- src/components/TechUsed.js
+++ src/components/TechUsed.js
@@ -10,121 +10,121 @@ export default function TechUsed() {
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-javascript-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-jquery-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-react-original"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-webpack-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-bower-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-nodejs-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-express-original"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-mongodb-original"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-angularjs-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-html5-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-css3-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-sass-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-bootstrap-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-git-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-github-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-gulp-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-sourcetree-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-ssh-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-wordpress-plain"
 				/>
 				<i
 					data-aos="zoom-in"
 					data-aos-duration="3000"
-					style={{ fontSize: '100px', margin: '5px' }}
+					style={{ fontSize: '3rem', margin: '5px' }}
 					className="devicon-chrome-plain"
 				/>
 			</div>
